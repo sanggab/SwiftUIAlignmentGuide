566ed8fdc7d95dc022826247212f4dbd3ce0221a
심상갑 <kappa2410@gmail.com>
2023-11-30 08:58:39 +0900

    Initial Commit

---
+++ SwiftUIAlignmentGuide/SwiftUIAlignmentGuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAlignmentGuideApp.swift
+//  SwiftUIAlignmentGuide
+//
+//  Created by Gab on 2023/11/30.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAlignmentGuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
